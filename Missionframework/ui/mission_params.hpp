889394e9765ba4b7d62ea253b2003ba002410891
Ewan <66959168+frazzleman10@users.noreply.github.com>
2025-11-03 10:39:36 +0000

    Updated mission parameters

--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -87,7 +87,7 @@ class Params {
         title = $STR_PARAMS_DAYDURATION;
         values[] = {8, 6, 4, 3, 2, 1};
         texts[] = {"3","4","6","8","12","24"};
-        default = 6;
+        default = 8;
     };
     class ShorterNights {
         title = $STR_SHORTER_NIGHTS_PARAM;
